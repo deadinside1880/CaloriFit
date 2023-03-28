ef8bfe682e212a9421bb52a34839f31476f2c72a
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2023-03-28 10:51:00 +0800

    Share Button functionality fixed

--- lib/screens/achievementdetails.dart
+++ lib/screens/achievementdetails.dart
@@ -2,6 +2,8 @@ import 'package:calori_fit/models/Achievement.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:wc_flutter_share/wc_flutter_share.dart';
 
 import '../styles/Colors.dart';
 
@@ -14,12 +16,11 @@ class AchievementDetails extends StatelessWidget {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.symmetric(
-          horizontal: MediaQuery.of(context).size.width/20, 
-          vertical: MediaQuery.of(context).size.height/20
-        ),
+            horizontal: MediaQuery.of(context).size.width / 20,
+            vertical: MediaQuery.of(context).size.height / 20),
         child: Column(
           children: [
-            Row( 
+            Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 GestureDetector(
@@ -28,25 +29,34 @@ class AchievementDetails extends StatelessWidget {
                     height: 40,
                     width: 40,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: grey
+                        borderRadius: BorderRadius.circular(20), color: grey),
+                    child: const Icon(
+                      Icons.keyboard_arrow_left_outlined,
+                      size: 25,
                     ),
-                    child: const Icon(Icons.keyboard_arrow_left_outlined, size: 25,),
                   ),
                 ),
                 Flexible(child: Container()),
                 Center(
-                  child: IconButton(
-                    onPressed: (){}, 
-                    icon: const Icon(Icons.share)
-                  )
-                ),
+                    child: IconButton(
+                        onPressed: () {
+                          Share.share(
+                              'Hey! Check my achievement out! I achieved ${achievement.title}.\nDownload CaloriFit to achieve your fitness goals! https://play.google.com/store/apps/details?id=com.calorifit');
+                        },
+                        icon: const Icon(Icons.share))),
                 //Flexible(child: Container()),
               ],
             ),
-            const SizedBox(height: 10,),
-            Text(achievement.title, style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
-            SizedBox(height: MediaQuery.of(context).size.height/10,),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              achievement.title,
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 10,
+            ),
             Hero(
               tag: 'trial${achievement.id}',
               child: CircleAvatar(
@@ -54,12 +64,18 @@ class AchievementDetails extends StatelessWidget {
                 radius: 150,
               ),
             ),
-            SizedBox(height: MediaQuery.of(context).size.height/10,),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 10,
+            ),
             const Text("CONGRATULATIONS!!!"),
-            const SizedBox(height: 10,),
-            const Text("You have completed this achievement! We are very proud of you for coming this far! We hope you continue your journey to glory with us!",
-             style: TextStyle(),
-             textAlign: TextAlign.center,),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "You have completed this achievement! We are very proud of you for coming this far! We hope you continue your journey to glory with us!",
+              style: TextStyle(),
+              textAlign: TextAlign.center,
+            ),
           ],
         ),
       ),
